0e98e4a6373beb163b1100d14ee7e81b1fd23b04
Maicon Brendon <112555118+maiconbre@users.noreply.github.com>
2025-02-25 12:28:44 -0300

    Update database.js

--- models/database.js
+++ models/database.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  protocol: 'postgres',
   dialectOptions: {
     ssl: {
       require: true,
@@ -10,5 +10,14 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     }
   }
 });
+
+// Testar a conexão
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conexão com o banco de dados estabelecida com sucesso.');
+  })
+  .catch(err => {
+    console.error('Não foi possível conectar ao banco de dados:', err);
+  });
 
 module.exports = sequelize;
