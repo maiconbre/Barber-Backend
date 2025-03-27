6106cd88680bb9d6eb90d84bfa8d231381a4f904
Maicon Brendon <112555118+maiconbre@users.noreply.github.com>
2025-03-27 17:43:31 -0300

    ok

--- models/database.js
+++ models/database.js
@@ -1,16 +1,18 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DATABASE_URL || 'postgresql://postgres:rDazZ1zCjD3PkOKJ@db.xxxsgvqbnkftoswascds.supabase.co:5432/postgres',
-  {
-    dialect: 'mysql',
-    dialectOptions: {
-      ssl: process.env.NODE_ENV === 'production' ? {
-        require: true,
-        rejectUnauthorized: false
-      } : false
+const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgresql://postgres:rDazZ1zCjD3PkOKJ@db.xxxsgvqbnkftoswascds.supabase.co:5432/postgres', {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
   }
-);
+});
 
 module.exports = sequelize;
